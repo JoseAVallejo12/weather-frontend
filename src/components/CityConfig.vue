897aaeda604a6072b1a3bc2f40b04fa32437e299
JoseAVallejo12 <josealfredovallejo25@gmail.com>
2022-12-10 17:42:23 -0500

    add prime UI library to the project

--- src/components/CityConfig.vue
+++ src/components/CityConfig.vue
@@ -1,29 +1,34 @@
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
 <template>
     <div class="config-container">
         <h1>City Config</h1>
         <div class="config-container--controls">
             <div class="config-container--controls__input">
-                <label for="cityName">
-                    <input id="cityName" type="text" placeholder="City Name" />
-                </label>
-                <button>Add City</button>
+                <span class="p-float-label p-input-icon-right">
+                    <i class="pi pi-search" />
+                    <InputText id="username" type="text" />
+                    <label for="username">Username</label>
+                </span>
             </div>
             <div class="config-container--controls__list">
                 <button>my cities</button>
             </div>
             <div class="config-container--controls__other">
-                <button>other cities</button>
+                <Button>other cities</Button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+
 export default {
     name: 'CityConfig',
     components: {
-        // CityConfig,
-        // CityCard,
+        Button,
+        InputText,
     },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+/* eslint-disable vue/multi-word-component-names */
+// import { createApp } from 'vue';
+// import PrimeVue from 'primevue/config';
+// import App from './App.vue';
 
-createApp(App).use(router).mount('#app');
+
+// createApp(App).use(router).use(PrimeVue).mount('#app');
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+// import Button from 'primevue/button';
+// import Toast from 'primevue/toast';
+// import ToastService from 'primevue/toastservice';
+import router from './router';
+import App from './App.vue'
+
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+app.use(PrimeVue);
+// app.use(ToastService);
+app.use(router);
+
+// app.component('Button', Button);
+// app.component('Toast', Toast);
+
+app.mount('#app');
